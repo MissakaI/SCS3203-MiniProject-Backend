19466cd8f15f7324f70f6c2993e087cd16206946
MissakaI <m.iddamalgoda@gmail.com>
2020-08-11 04:28:46 +0530

    entity almost complete

---
+++ src/main/java/lk/ac/cmb/ucsc/scs3203/miniproject/backend/models/Teacher.java
@@ -0,0 +1,13 @@
+package lk.ac.cmb.ucsc.scs3203.miniproject.backend.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Teacher extends User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    int id;
+}
